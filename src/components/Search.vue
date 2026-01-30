cda0dc56f2473f64dbcea0f73bfcd1fc4076a4d1
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-01-30 15:42:54 +0700

    10% about component to fix weather card

---
+++ src/components/Search.vue
@@ -0,0 +1,3 @@
+<template>
+    <input type="text" name="search" id="search" placeholder="text">
+</template>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import Search from './Search.vue'
+    defineProps ({
+        location :String,
+        temp :String,
+        icon : String,
+        color : String,
+        windspeed : Number,
+    });
+</script>
+<template>
+    <Search/>
+    <span>{{ location }}</span>
+</template>
