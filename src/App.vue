4e71a1772bae17ac3249e90f14dfcedfc7a88fbb
nimol <limnimol.dev@gmail.com>
2026-01-31 13:10:41 +0700

    pull feat/header and feat/footer successfull

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
-<script setup>
-import { RouterView } from 'vue-router';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import HelloWorld from './components/HelloWorld.vue'; 
-</script>
 
-<template>
-  <Header />
-  <RouterView />
-  <Footer />
-</template>
   <script setup>
-  feat/register
   import { RouterView }  from 'vue-router';
   import Header from '@/components/Header.vue';
   import Footer from '@/components/Footer.vue';
   import TheWelcome from '@/components/TheWelcome.vue';
   import RegisterView from './views/RegisterView.vue';
   </script>
->
+
 
 
   <template>
 
 
-    <RegisterView/>
+    
     <Header/>
-    <Footer />
     <RouterView />
+    <Footer />
+    
   </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <template>
   <main class="max-w-[1280px] mx-auto py-0 px-22">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
 
     <TheWelcome />
     <RouterView />
   </main>
-  <RouterView />
- 
 </template>
