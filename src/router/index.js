37318c265e6a4a552f8d4ed9b545f2b63037eb85
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-02-02 16:48:29 +0700

    newpage is 60%

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WeatherCardView from '../views/WeatherCardView.vue'
+import NewPageView from '../views/NewPageView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/weathercard',
       name: 'weathercard',
       component: WeatherCardView,
+    },
+    {
+      path: '/New',
+      name: 'Newpage',
+      component: NewPageView,
     },
   ],
 })
---
+++ src/views/NewPageView.vue
@@ -0,0 +1,145 @@
+<template>
+<body class="bg-[#e9ecef] text-gray-300">
+
+<div class="max-w-7xl mx-auto px-6 py-10">
+
+<!-- GRID -->
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-8">
+
+<!-- ================= MAIN CONTENT ================= -->
+<main class="lg:col-span-2 space-y-14">
+
+<!-- POST 1 -->
+<article>
+<h1 class="text-3xl text-black font-semibold mb-4">
+The Weather Of Phnom Penh City
+</h1>
+
+<img
+src="/img/Rain31016.jpg"
+class="rounded-lg w-full mb-4 h-[400px]"
+>
+
+<p class="text-black mb-4">
+Phnom Penh Storm Alert: Severe thunderstorm expected today with heavy rain and strong winds.
+Advice: Stay indoors, secure outdoor items, and expect possible localized flooding.
+</p>
+
+<button class="bg-sky-500 hover:bg-sky-600 text-white px-6 py-2 rounded-full">
+Read more
+</button>
+</article>
+
+<hr class="border-slate-700">
+
+<!-- POST 2 (STORM) -->
+<article>
+    <h1 class="text-3xl text-black font-semibold mb-4">
+The Weather Of Cambodia
+</h1>
+<img
+src="/img/cambodia-rain-.jpg"
+class="rounded-lg w-full mb-4 h-[400px]"
+>
+
+<p class="text-black mb-4">
+Cambodia Storm Alert: Strong thunderstorm expected today with heavy rain and winds.
+Advice: Stay indoors, secure outdoor items, and watch for possible flooding.
+</p>
+
+<button class="bg-sky-500 hover:bg-sky-600 text-white px-6 py-2 rounded-full">
+Read more
+</button>
+</article>
+
+<hr class="border-slate-700">
+
+<!-- POST 3 (SPACE) -->
+<article>
+<h1 class="text-3xl text-black font-semibold mb-4">
+Watch From Space When Stom In The Asia
+</h1>
+
+<img
+src="/img/space.webp"
+class="rounded-lg w-full mb-4 h-[400px]"
+>
+
+<p class="text-black">
+Asia Storm Alert: A severe thunderstorm is expected today across parts of the country, bringing heavy rainfall, strong
+winds, and occasional lightning. Weather conditions may reduce visibility, and localized flooding is possible in low-lying areas.
+</p>
+<br>
+<button class="bg-sky-500 hover:bg-sky-600 text-white px-6 py-2 rounded-full">
+Read more
+</button>
+
+</article>
+
+
+</main>
+
+<!-- ================= SIDEBAR ================= -->
+<aside class="space-y-8">
+
+<!-- CATEGORIES -->
+<div class="bg-slate-800 p-6 rounded-lg">
+<h2 class="text-xl text-white font-semibold mb-4">Categories</h2>
+<ul class="space-y-3">
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Dolores ratione</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Voluptatem nesciunt</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Neque porro quisquam</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Dolorem ipsum quia</li>
+</ul>
+</div>
+
+<!-- HOT NEWS -->
+<div class="bg-slate-800 p-6 rounded-lg">
+<h2 class="text-xl text-white font-semibold mb-4">Hot News</h2>
+<ul class="space-y-3">
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Accusamus dignissimos</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Ducimus praesentium</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Voluptatum deleniti</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Occaecati provident</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Dolors rations</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Voluptatem nesciunt</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Neque porro quisquam</li>
+<li class="text-sky-400 hover:text-sky-300 cursor-pointer">→ Dolorem ipsum qua</li>
+</ul>
+</div>
+
+<!-- TOP RATED POSTS -->
+<div class="bg-slate-800 p-6 rounded-lg">
+<h2 class="text-xl text-white font-semibold mb-4">Top rated posts</h2>
+
+<ul class="space-y-4">
+<li>
+<p class="text-gray-300">Doloremque laudantium lorem</p>
+<span class="text-sky-400 font-semibold">5.5</span>
+<span class="text-gray-500 text-sm">(759 rates)</span>
+</li>
+
+<li>
+<p class="text-gray-300">Doloremque laudantium lorem</p>
+<span class="text-sky-400 font-semibold">5.5</span>
+<span class="text-gray-500 text-sm">(759 rates)</span>
+</li>
+
+<li>
+<p class="text-gray-300">Doloremque laudantium lorem</p>
+<span class="text-sky-400 font-semibold">5.5</span>
+<span class="text-gray-500 text-sm">(759 rates)</span>
+</li>
+</ul>
+</div>
+
+</aside>
+
+</div>
+</div>
+
+</body>
+
+
+
+</template>
