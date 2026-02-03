1450cc12dad6b28d17b3ed5f4f9a7886d5eeec51
nimol <limnimol.dev@gmail.com>
2026-02-03 15:05:37 +0700

    fix again all of error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from '@/components/Header.vue';
-  import Footer from '@/components/Footer.vue';
+import Footer from '@/components/Footer.vue';
+import RegisterView from './views/RegisterView.vue';
+
 </script>
 
 <template>
