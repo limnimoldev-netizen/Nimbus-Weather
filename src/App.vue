d00e5d7b198f2f86da186462c1240fbb78afe547
limnimoldev-netizen <git config --global user.email limnimol.dev@gmail.com>
2026-02-03 23:14:35 +0700

    Finaly i fix success on branch main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
-import RegisterView from './views/RegisterView.vue';
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 </script>
 
 
-
-  <template>
-    <Header/>
-    <RouterView />
-
-    <Footer />
-  </template>
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
