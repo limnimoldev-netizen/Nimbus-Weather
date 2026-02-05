71b772f294739cae6c5764390437eef00742b05d
nimol <limnimol.dev@gmail.com>
2026-02-05 13:28:25 +0700

    fix what i pull

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
- information
+ 
 import WeatherCardView from '../views/WeatherCardView.vue'
 import NewPageView from '../views/NewPageView.vue'
 import InformationPageView from '../views/InformationPageView.vue'
-information2
 import Information1PageView from '../views/Information1PageView.vue'
 import Information2PageView from '../views/Information2PageView.vue'
-=======
 
 import ContactView from '../views/ContactView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import WeatherView from '../views/WeatherView.vue'
 
- main
- main
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
     {
       path: '/',
       name: 'home',
       component: HomeView,
     },
+
     {
       path: '/about',
       name: 'about',
@@ -32,35 +31,40 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
     {
- information
+ 
       path: '/weathercard',
       name: 'weathercard',
       component: WeatherCardView,
     },
+
     {
       path: '/New',
       name: 'newpage',
       component: NewPageView,
     },
+
     {
       path: '/infor',
       name: 'information',
       component: InformationPageView,
     },
- information2
+ 
+
     {
       path: '/infor1',
       name: 'information1',
       component: Information1PageView,
     },
+
     {
       path: '/infor2',
       name: 'information2',
       component: Information2PageView,
     },
 
-
+    {
       path: '/sitting',
       name: 'sitting',
       component: () => import('../views/SettingView.vue'),
@@ -91,8 +95,6 @@ const router = createRouter({
     
     },
  
-main
- main
   ],
 })
 
