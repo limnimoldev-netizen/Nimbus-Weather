e7345801761d3bdcf13e3e4eb99cc730581b4378
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-02-09 09:58:12 +0700

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
- information
 import WeatherCardView from '../views/WeatherCardView.vue'
 import NewPageView from '../views/NewPageView.vue'
 import InformationPageView from '../views/InformationPageView.vue'
-information2
 import Information1PageView from '../views/Information1PageView.vue'
 import Information2PageView from '../views/Information2PageView.vue'
-=======
 
-import ContactView from '../views/ContactView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import LoginView from '../views/LoginView.vue'
-import WeatherView from '../views/WeatherView.vue'
-
- main
- main
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,75 +15,66 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
     {
- information
       path: '/weathercard',
       name: 'weathercard',
       component: WeatherCardView,
     },
+
     {
-      path: '/New',
+      path: '/new',
       name: 'newpage',
       component: NewPageView,
     },
+
     {
       path: '/infor',
       name: 'information',
       component: InformationPageView,
     },
- information2
+
     {
       path: '/infor1',
       name: 'information1',
       component: Information1PageView,
     },
+
     {
       path: '/infor2',
       name: 'information2',
       component: Information2PageView,
     },
 
-
-      path: '/sitting',
-      name: 'sitting',
+    {
+      path: '/setting',
+      name: 'setting',
       component: () => import('../views/SettingView.vue'),
     },
 
-
     {
-      
-      path: "/register",
+      path: '/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
-    
     },
 
     {
-      
-      path: "/login",
+      path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue'),
-    
     },
 
     {
-      
-      path: "/weather",
+      path: '/weather',
       name: 'weather',
       component: () => import('../views/WeatherView.vue'),
-    
     },
- 
-main
- main
   ],
 })
 
