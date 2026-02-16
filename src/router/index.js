a9c2c44fd0bedf779907f3b8c14db31354bf7bc2
Neath <neath@gmail.com>
2026-02-16 14:32:58 +0700

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import HomeView from '../views/HomeView.vue'
-import WeatherCardView from '../views/WeatherCardView.vue'
 import NewPageView from '../views/NewPageView.vue'
 import InformationPageView from '../views/InformationPageView.vue'
 import Information1PageView from '../views/Information1PageView.vue'
@@ -15,67 +13,23 @@ import WeatherView from '../views/WeatherView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
-
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-
-    {
-      path: '/weathercard',
-      name: 'weathercard',
-      component: WeatherCardView,
-    },
-
-    {
-      path: '/new',
-      name: 'newpage',
-      component: NewPageView,
-    },
-
-    {
-      path: '/infor',
-      name: 'information',
-      component: InformationPageView,
-    },
-
-    {
-      path: '/infor1',
-      name: 'information1',
-      component: Information1PageView,
-    },
-
-    {
-      path: '/infor2',
-      name: 'information2',
-      component: Information2PageView,
-    },
-
-    {
-      path: '/setting',
-      name: 'setting',
-      component: () => import('../views/SettingView.vue'),
-    },
-
+    
 
     {
       
       path: "/",
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
+    
     },
 
 
     {
-      path: '/login',
+      
+      path: "/login",
       name: 'login',
       component: () => import('../views/LoginView.vue'),
+    
     },
 
 
@@ -130,9 +84,11 @@ const router = createRouter({
 
     
     {
-      path: '/weather',
+      
+      path: "/weather",
       name: 'weather',
       component: () => import('../views/WeatherView.vue'),
+    
     },
 
     {
